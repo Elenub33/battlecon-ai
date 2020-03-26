03f2ed318322d64135c7cbdd86cbbf3bbcf4df4a
Charles Tam <rngesus.wept+github@gmail.com>
2020-03-26 10:45:12 -0400

    typing

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
-def IsOrdered(a, b, c):
+from typing import List
+
+
+def IsOrdered(a, b, c) -> bool:
   """Returns True iff A < B < C OR A > B > C."""
   return (a - b) * (c - b) < 0
 
-def PositionsBetween(a, b):
+
+def PositionsBetween(a, b) -> List[int]:
   """Returns the board positions between spaces A and B inclusive."""
   # TODO: Python 2->3 migration revealed an issue where some positions or
   # ranges are floats instead of ints. This is a problem Python3's range() does
