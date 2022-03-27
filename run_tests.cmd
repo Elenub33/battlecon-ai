e2a220d5af1dc730ce58dcad70712e61e682a2a2
Joshua Broberg <joshua.broberg@d2ktech.com>
2022-03-27 14:56:43 -0700

    Add pipenv and test coverage checking, switch from sh to cmd for scripts

---
+++ run_tests.cmd
@@ -0,0 +1,3 @@
+set SCRIPT_DIR=%~dp0
+
+pipenv run python -m unittest discover %SCRIPT_DIR%test 1> test_stdout.txt 2> test_results.txt
--- run_tests.sh
+++
@@ -1,3 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-python -m unittest discover $SCRIPT_DIR/test 1> test_stdout.txt 2> test_results.txt
