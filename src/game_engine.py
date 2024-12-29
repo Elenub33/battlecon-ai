6d887dfe4b64ecb3e3cbec380a8e17ebb5f59789
Joshua Broberg <joshuadbroberg@gmail.com>
2024-12-29 00:19:07 -0800

    Complete basic Game Engine

--- src/game_engine.py
+++ src/game_engine.py
@@ -23,16 +23,17 @@ class GameEngine:
         raise NotImplementedError()
     
 
-    def run(self, game_state: GameState):
+    def run(self):
+        game_state = self._get_game_state()
         if game_state == None:
             return ValueError("GameEngine not initialized before call to run().")
         psm = PhaseStateMachine()
         while not game_state.is_over():
-            psm.do_next(self.get_game_state())
+            psm.do_next(self._get_game_state())
 
     
     def get_active_agent(self) -> Agent:
-        active_fighter = self.get_game_state().get_active_fighter()
+        active_fighter = self._get_game_state().get_active_fighter()
         for agent in self.agents:
             if agent.get_fighter() == active_fighter:
                 return agent
@@ -40,8 +41,17 @@ class GameEngine:
     
     
     def get_reactive_agent(self) -> Agent:
-        active_fighter = self.get_game_state().get_reactive_fighter()
+        active_fighter = self._get_game_state().get_reactive_fighter()
         for agent in self.agents:
             if agent.get_fighter() == active_fighter:
                 return agent
         raise Exception("Unable to find reactive agent.")
+
+
+    def _set_game_state(self, game_state: GameState):
+        self._game_state = game_state
+        
+        
+    def _get_game_state(self) -> GameState:
+        return self._game_state
+        
